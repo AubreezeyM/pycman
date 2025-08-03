f999ba7eadaac869ca8914c03b08193dba3ef78f
Aubrey <aubreyminahan@gmail.com>
2025-08-02 21:07:22 -0400

    better i think

--- engine/systems.py
+++ engine/systems.py
@@ -6,17 +6,12 @@ from .components import Component, SpriteComponent
 from .entity import Entity
 
 class System(abc.ABC):
-    _tracked_components: set[type[Component]] = set()
-
-    @classmethod
-    def track_component(cls, component_type: type[Component]) -> None:
-        cls._tracked_components.add(component_type)
+    pass
 
 class RenderSystem(System):
     def __init__(self, screen: pygame.Surface):
         self.entities = set()
         self.screen: pygame.Surface = screen
-        self.track_component(SpriteComponent)
 
     def update(self) -> None:
         for entity in self.entities:
@@ -24,7 +19,7 @@ class RenderSystem(System):
             self.screen.blit(sprite.sprite, (sprite.x, sprite.y))
 
     def add(self, entity: Entity) -> None:
-        if all(entity.get_component(t) for t in self._tracked_components):
+        if all(entity.get_component(t) for t in self.entities):
             self.entities.add(entity)
 
 
